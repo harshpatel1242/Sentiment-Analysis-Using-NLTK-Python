87885cb09c05367370c956994c02a85e3b33c4f8
harsh <harshpatel1242@gmail.com>
2021-04-06 17:21:10 +0530

    Sentiment First Commit

--- faculty/Chart.js-master/src/plugins/index.js
+++
@@ -1,4 +0,0 @@
-export {default as filler} from './plugin.filler';
-export {default as legend} from './plugin.legend';
-export {default as title} from './plugin.title';
-export {default as tooltip} from './plugin.tooltip';
--- faculty/web/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['login_user']);
-unset($_SESSION['login']);
-header("location:index.php");
-?>
